5ee47112ea7658031ae23c40396cad2716962bd5
sergioarg <sergio_ramos53@yahoo.com>
2024-05-10 13:37:01 -0500

    Removeing extra conditional

--- loans/views.py
+++ loans/views.py
@@ -17,6 +17,7 @@ class LoansViewSet(viewsets.ModelViewSet):
     # permission_classes = (permissions.IsAuthenticated,)
 
     def create(self, request, *args, **kwargs):
+        # Default validatiosn of serializer
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
@@ -24,24 +25,24 @@ class LoansViewSet(viewsets.ModelViewSet):
         customer = Customer.objects.get(pk=customer_id)
 
         # Check if customer has amount available to request loan
-        if customer:
-            total_amount = Loan.objects.filter(
-                customer=customer,
-                status__in=(0, 1)
-            ).aggregate(total_amount=models.Sum('amount')).get('total_amount', 0)
+        total_amount = Loan.objects.filter(
+            customer=customer,
+            status__in=(0, 1)
+        ).aggregate(total_amount=models.Sum('amount')
+        ).get('total_amount', 0)
 
-            if not total_amount:
-                total_amount = 0
+        if not total_amount:
+            total_amount = 0
 
-            amount = Decimal(request.data.get('amount'))
+        amount = Decimal(request.data.get('amount'))
 
-            if total_amount + amount > customer.score:
-                response = {
-                    "detail": f"You cannot create a loan greater than {customer.score} your current debt is {total_amount}."
-                }
-                status_code = status.HTTP_400_BAD_REQUEST
+        if total_amount + amount > customer.score:
+            response = {
+                "detail": f"You cannot create a loan greater than {customer.score} your current debt is {total_amount}."
+            }
+            status_code = status.HTTP_400_BAD_REQUEST
 
-                return Response(response, status_code)
+            return Response(response, status_code)
 
         response = super().create(request, *args, **kwargs)
         return response
