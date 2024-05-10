a9ffbdf0603c6035c46245abd3c2aa71877c3cb8
sergioarg <sergio_ramos53@yahoo.com>
2024-05-10 18:58:21 -0500

    Creating validation validate amount

--- loans/serializers.py
+++ loans/serializers.py
@@ -1,7 +1,10 @@
 # pylint: disable=E1101
 """ Module Serilizers """
+from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 from .models import Loan
+from customers.models import Customer
 
 class LoanSerializer(serializers.ModelSerializer):
     """
@@ -23,6 +26,43 @@ class LoanSerializer(serializers.ModelSerializer):
             'contract_version': {'write_only': True}
         }
         read_only_fields = ("outstanding",)
+
+    def validate_amount(self, amount):
+        """Validate amount requested could be requested
+
+        Args:
+            amount (Decimal): amount requested
+
+        Raises:
+            serializers.ValidationError: amount is greater than score
+
+        Returns:
+            Decimal: amount requested
+        """
+        customer_id = self.initial_data.get('customer')
+        if customer_id:
+            try:
+                customer = Customer.objects.get(id=customer_id)
+            except ObjectDoesNotExist as exc:
+                _ = exc
+                raise serializers.ValidationError("Customer not found.")
+
+            credit_available = customer.score
+            total_amount = Loan.objects.filter(
+                customer=customer,
+                status__in=(0, 1)
+            ).aggregate(total_amount=models.Sum('amount')
+            ).get('total_amount', 0)
+
+            if not total_amount:
+                total_amount = 0
+
+            if total_amount + amount > credit_available:
+                raise serializers.ValidationError({
+                    "detail": f"You cannot create a loan greater than {credit_available} your current debt is {total_amount}."
+                })
+        return amount
+
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
--- loans/views.py
+++ loans/views.py
@@ -1,9 +1,5 @@
 """ Module with CustomerViewSet """
-from decimal import Decimal
-from rest_framework import viewsets, permissions, status
-from rest_framework.response import Response
-from django.db import models
-from customers.models import Customer
+from rest_framework import viewsets
 from .models import Loan
 from .serializers import LoanSerializer
 
@@ -15,34 +11,3 @@ class LoansViewSet(viewsets.ModelViewSet):
     queryset = Loan.objects.all().order_by('id')
     serializer_class = LoanSerializer
     # permission_classes = (permissions.IsAuthenticated,)
-
-    def create(self, request, *args, **kwargs):
-        # Default validatiosn of serializer
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        customer_id = request.data.get('customer')
-        customer = Customer.objects.get(pk=customer_id)
-
-        # Check if customer has amount available to request loan
-        total_amount = Loan.objects.filter(
-            customer=customer,
-            status__in=(0, 1)
-        ).aggregate(total_amount=models.Sum('amount')
-        ).get('total_amount', 0)
-
-        if not total_amount:
-            total_amount = 0
-
-        amount = Decimal(request.data.get('amount'))
-
-        if total_amount + amount > customer.score:
-            response = {
-                "detail": f"You cannot create a loan greater than {customer.score} your current debt is {total_amount}."
-            }
-            status_code = status.HTTP_400_BAD_REQUEST
-
-            return Response(response, status_code)
-
-        response = super().create(request, *args, **kwargs)
-        return response
