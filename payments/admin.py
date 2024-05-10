c02951a7509fa65df07e7f654bf3ca27c1950719
sergioarg <sergio_ramos53@yahoo.com>
2024-05-10 16:26:14 -0500

    Cleaning code

--- payments/admin.py
+++ payments/admin.py
@@ -1,16 +1,16 @@
+""" Register apps and model Payment in admin """
 from django.contrib import admin
-from .models import Payment
+from .models import Payment, PaymentLoanDetail
 
 class PaymentsAdmin(admin.ModelAdmin):
     """ Configurations of Admin panel """
 
-    # list_display = (
-    #     'id', 'external_id',
-    #     'status', 'score', 'created_at',
-    #     'updated_at', 'preapproved_at'
-    # )
+    list_display = (
+        'id', 'total_amount', 'status',
+    )
 
-    search_fields = ('external_id', 'status', 'score')
+    search_fields = ('external_id', 'status', 'payment_at')
 
 # Register your models here.
 admin.site.register(Payment, PaymentsAdmin)
+admin.site.register(PaymentLoanDetail)
