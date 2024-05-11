f1e8eeda9c6d71cb78aa15b05b3da5c80523f616
sergioarg <sergio_ramos53@yahoo.com>
2024-05-10 20:11:19 -0500

    Create constans file

---
+++ constans.py
@@ -0,0 +1,18 @@
+"""  Constans of statuses """
+
+CUSTOMER_STATUS = {
+    "COMPLETED": 1,
+    "REJECTED": 2
+}
+
+USER_STATUS = {
+    "ACTIVE": 1,
+    "INACTIVE": 2
+}
+
+LOANS_STATUS = {
+    "PENDING": 1,
+    "ACTIVE": 2,
+    "REJECTED": 3,
+    "PAID": 4
+}
